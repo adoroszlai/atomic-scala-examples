30cea4cff38229057ebbf36299af5e7fc6962af2
Doroszlai Attila <eti@fene.hu>
2014-05-03 20:49:28 +0200

    Constructors 3

--- examples/Tea.scala
+++ examples/Tea.scala
@@ -30,15 +30,19 @@ class Tea(val decaf:Boolean = false,
 val tea = new Tea
 tea.describe is "Earl Grey"
 tea.calories is 0
+tea.name is "Earl Grey"
 
 val lemonZinger = new Tea(decaf=true, name="Lemon Zinger")
 lemonZinger.describe is "Lemon Zinger decaf"
 lemonZinger.calories is 0
+lemonZinger.decaf is true
 
 val sweetGreen = new Tea(name="Jasmine Green", sugar=true)
 sweetGreen.describe is "Jasmine Green + sugar"
 sweetGreen.calories is 16
+sweetGreen.sugar is true
 
 val teaLatte = new Tea(sugar=true, milk=true)
 teaLatte.describe is "Earl Grey + milk + sugar"
 teaLatte.calories is 116
+teaLatte.milk is true
