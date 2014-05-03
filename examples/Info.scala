e2373fe04ee8007547daf12a9cdf9b6f346cd75e
Doroszlai Attila <eti@fene.hu>
2014-05-03 20:58:15 +0200

    class exc. 3

--- examples/Info.scala
+++ examples/Info.scala
@@ -1,9 +1,11 @@
 import com.atomicscala.AtomicTest._
 
-class Info(val name:String, var description:String)
+class Info(var name:String, var description:String)
 
 val info = new Info("stuff", "Something")
 info.name is "stuff"
 info.description is "Something"
 info.description = "Something else"
 info.description is "Something else"
+info.name = "This is the new name"
+info.name is "This is the new name"
