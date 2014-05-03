0b808512f2fd0d5b58c734eade7522a166656b64
Doroszlai Attila <eti@fene.hu>
2014-05-03 20:28:21 +0200

    Pattern Matching

--- examples/CheckTruth.scala
+++ examples/CheckTruth.scala
@@ -3,25 +3,21 @@ import com.atomicscala.AtomicTest._
 
 def checkTruth(
   exp1:Boolean, exp2:Boolean):String = {
-  if(exp1 && exp2) {
-    "Both are true"
-  }
-  else if(!exp1 && !exp2) {
-    "Both are false"
-  }
-  else if(exp1) {
-    "First: true, second: false"
-  }
-  else {
-    "First: false, second: true"
-  }
+		exp1 match {
+			case true => 
+				exp2 match {
+					case true => "Both are true"
+					case false => "First: true, second: false"
+				}
+			case false =>
+				exp2 match {
+					case true => "First: false, second: true"
+					case false => "Both are false"
+				}
+		}
 }
 
-checkTruth(true || false, true) is
-  "Both are true"
-checkTruth(1 > 0 && -1 < 0, 1 == 2) is
-  "First: true, second: false"
-checkTruth(1 >= 2, 1 >= 1) is
-  "First: false, second: true"
-checkTruth(true && false,false && true) is
-"Both are false"
+checkTruth(true || false, true) is "Both are true"
+checkTruth(1 > 0 && -1 < 0, 1 == 2) is "First: true, second: false"
+checkTruth(1 >= 2, 1 >= 1) is "First: false, second: true"
+checkTruth(true && false,false && true) is "Both are false"
